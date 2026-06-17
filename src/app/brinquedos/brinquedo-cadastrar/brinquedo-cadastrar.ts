b839accbfd2f43ce00d05d7077a27570146d5909
Fabio Romão <41930@proway.treina>
2026-06-17 19:54:22 -0300

    brinquedo-cadastrar

--- src/app/brinquedos/brinquedo-cadastrar/brinquedo-cadastrar.ts
+++ src/app/brinquedos/brinquedo-cadastrar/brinquedo-cadastrar.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrinquedoModel } from '../../models/brinquedo.model';
 
 @Component({
   selector: 'app-brinquedo-cadastrar',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './brinquedo-cadastrar.html',
   styleUrl: './brinquedo-cadastrar.scss',
 })
-export class BrinquedoCadastrar {}
+export class BrinquedoCadastrar {
+  brinquedo = signal<BrinquedoModel>({
+    id: '',
+    marca: '',
+    modelo: '',
+    quantidade: null,
+    preco: null
+  });
+}
