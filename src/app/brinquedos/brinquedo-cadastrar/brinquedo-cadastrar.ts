38ba1f62a662bf01cc6b9fa19ea5cec4dd04cda3
Fabio Romão <41930@proway.treina>
2026-06-17 20:37:09 -0300

    cadastro-completo

--- src/app/brinquedos/brinquedo-cadastrar/brinquedo-cadastrar.ts
+++ src/app/brinquedos/brinquedo-cadastrar/brinquedo-cadastrar.ts
@@ -16,4 +16,26 @@ export class BrinquedoCadastrar {
     quantidade: null,
     preco: null
   });
+
+  salvar(): void {
+    const brinquedos = this.carregarBrinquedosDoLocalStorage();
+
+    brinquedos.push(this.brinquedo());
+
+    const brinquedoString = JSON.stringify(brinquedos);
+    localStorage.setItem("brinquedos", brinquedoString)
+
+    alert("Brinquedo cadastrado com sucesso");
+
+  }
+
+  carregarBrinquedosDoLocalStorage(): BrinquedoModel[] {
+    const brinquedosString = localStorage.getItem("Brinquedos");
+
+    if (brinquedosString === null) {
+      return [];
+    }
+
+    return JSON.parse(brinquedosString) as BrinquedoModel[];
+  }
 }
